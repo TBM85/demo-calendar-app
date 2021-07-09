6ac6ed8958db1c435e5fdea4992e6876b8276fe8
TBM85 <tballesterm@gmail.com>
2021-07-09 11:37:06 -0400

    Displays an array with the days of the month

--- src/components/Calendar/CalendarBody/CalendarBody.js
+++ src/components/Calendar/CalendarBody/CalendarBody.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 import classes from "./CalendarBody.module.scss";
 
 const CalendarBody = (props) => {
+  const { daysInMonth } = props;
+
+  const [dayNumber, setDayNumber] = useState([]);
+  
+  useEffect(() => {
+    let daysInMonthArray = [];
+
+    for (let i = 1; i <= daysInMonth; i++) {
+      const dayNumber = i;
+  
+      daysInMonthArray.push({
+        id: uuidv4(),
+        day: dayNumber
+      })
+    }
+
+    setDayNumber(daysInMonthArray);
+  }, [daysInMonth])
+  
   return (
     <div className={classes.calendar_body}>
       <ul className={classes["week-days"]}>
@@ -10,7 +30,11 @@ const CalendarBody = (props) => {
           <li key={index}>{props.isBigDevice ? day : day.slice(0, 2)}</li>
         ))}
       </ul>
-      <div className={classes["month-days"]}>{props.daysInMonth}</div>
+      <div className={classes["month-days"]}>
+        {dayNumber.map((number) => (
+          <div key={number.id}>{number.day}</div>
+        ))}
+      </div>
     </div>
   );
 };
