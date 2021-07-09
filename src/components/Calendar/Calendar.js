fa37a12ba071f3dc5ae7c4ace529af4a2cf97408
TBM85 <tballesterm@gmail.com>
2021-07-09 10:14:51 -0400

    Reduce the day name for small devices

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from "./Calendar.module.scss";
 import CalendarHeader from "./CalendarHeader/CalendarHeader";
@@ -14,15 +14,34 @@ const weekdays = [
 ];
 
 const Calendar = () => {
+  // Handle media queries
+  const [isBigDevice, setIsBigDevice] = useState("");
+
+  const changeDevicesHandler = (bigDevice) => {
+    setIsBigDevice(bigDevice.matches);
+  };
+
+  useEffect(() => {
+    const bigDevice = window.matchMedia("(min-width: 992px)");
+    bigDevice.addEventListener("change", changeDevicesHandler);
+    changeDevicesHandler(bigDevice);
+
+    return () => {
+      bigDevice.removeEventListener("change", changeDevicesHandler);
+    };
+  }, []);
+
   return (
     <div className={classes.calendar}>
       <CalendarHeader />
-      <ul className={classes["week-days"]}>
-        {weekdays.map((day, index) => (
-          <li key={index}>{day}</li>
-        ))}
-      </ul>
-      <div className={classes["month-days"]}></div>
+      <div className={classes.calendar_body}>
+        <ul className={classes["week-days"]}>
+          {weekdays.map((day, index) => (
+            <li key={index}>{isBigDevice ? day : day.slice(0, 2)}</li>
+          ))}
+        </ul>
+        <div className={classes["month-days"]}></div>
+      </div>
     </div>
   );
 };
