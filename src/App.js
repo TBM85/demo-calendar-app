628b7af3fdc05e47e336a9703946dbd42d5e7149
TBM85 <tballesterm@gmail.com>
2021-07-10 09:32:04 -0400

    Create a Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Calendar from './components/Calendar/Calendar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Calendar />
+      <Footer />
     </div>
   );
 }
